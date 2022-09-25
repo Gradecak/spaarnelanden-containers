156c3b3088dd2b5bf81cef93c3bd1eb87c9bf68b
Marijan Gradecak <maki.gradecak@bloomon.nl>
2022-09-25 15:27:29 +0200

    Refactor entity to use co-ordinated API fetching and entity updates

--- custom_components/spaarnelanden/sensor.py
+++ custom_components/spaarnelanden/sensor.py
@@ -1,15 +1,17 @@
 from datetime import datetime, timedelta
-from functools import lru_cache, partial
 import json
 import logging
 import re
-import time
 from typing import Any, Dict
 
 from bs4 import BeautifulSoup
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 import requests
 import voluptuous as vol
 
@@ -22,39 +24,45 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-
-SCAN_INTERVAL = timedelta(hours=2)
-
 CONTAINER_INFO_RE = r"var oContainerModel =(\[{.*?}\]);"
 DATE_RE = r"Date\((.*?)\)"
 
 log = logging.getLogger(__name__)
 
 
-def get_ttl_hash(seconds=3600):
-    round(time.time() / seconds)
-
-
-@lru_cache
-def fetch_data(ttl_key=None):
-    resp = requests.get("https://inzameling.spaarnelanden.nl/")
-    soup = BeautifulSoup(resp.text, "html.parser")
-    script_ele = soup.find(id="MapPartial").findChild(type="text/javascript")
-    if script_ele is None:
-        log.error("Failed to find script container")
-        raise ValueError("Failed to find script container")
-    data_search = re.search(CONTAINER_INFO_RE, script_ele.get_text())
-    if data_search is None:
-        log.error("Failed to find data in script")
-        raise ValueError("failed to extract container data from script")
-
-    return json.loads(data_search.group(1))
-
-
-class ContainerSensor(Entity):
-    def __init__(self, container_id: str, hass):
-        super().__init__()
+class SpaarnelandenCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass):
+        super().__init__(
+            hass,
+            log,
+            name="spaarnelanden",
+            update_interval=timedelta(hours=1)
+        )
         self.hass = hass
+
+    def _fetch_data(self):
+        resp = requests.get("https://inzameling.spaarnelanden.nl/")
+        soup = BeautifulSoup(resp.text, "html.parser")
+        script_ele = soup.find(id="MapPartial").findChild(type="text/javascript")
+        if script_ele is None:
+            log.error("Failed to find script container")
+            raise ValueError("Failed to find script container")
+        data_search = re.search(CONTAINER_INFO_RE, script_ele.get_text())
+        if data_search is None:
+            log.error("Failed to find data in script")
+            raise ValueError("failed to extract container data from script")
+
+        return json.loads(data_search.group(1))
+
+    async def _async_update_data(self):
+        log.info("Fetching container state from saarnelanden")
+        data = await self.hass.async_add_executor_job(self._fetch_data)
+        return {c['sRegistrationNumber']: c for c in data}
+
+
+class ContainerSensor(CoordinatorEntity):
+    def __init__(self, coordinator, container_id: str):
+        super().__init__(coordinator)
         self.container_id = container_id
         self.attrs = {const.ATTR_CAPACITY: 0, const.ATTR_ID: container_id}
         self._state = None
@@ -80,13 +88,9 @@ class ContainerSensor(Entity):
     def extra_state_attributes(self) -> Dict[str, Any]:
         return self.attrs
 
-    async def async_update(self):
-        data_fetcher = partial(fetch_data, get_ttl_hash())
-        data = await self.hass.async_add_executor_job(data_fetcher)
-        [container, *_] = [
-            c for c in data if c["sRegistrationNumber"] == self.container_id
-        ]
-
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        container = self.coordinator.data[self.container_id]
         last_emptied_match = re.search(DATE_RE, container["dtDateLastEmptied"])
         last_emptied = int(last_emptied_match.group(1)) / 1000
         self.attrs = {
@@ -98,12 +102,16 @@ class ContainerSensor(Entity):
         }
         self._available = True
         self._state = container["dFillingDegree"]
+        self.async_write_ha_state()
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     log.debug("Setting up spaarnelanden container sensor")
     containers = set(config.get(CONF_CONTAINERS))
+    coordinator = SpaarnelandenCoordinator(hass)
+    await coordinator.async_config_entry_first_refresh()
+
     async_add_entities(
-        [ContainerSensor(container_id, hass) for container_id in containers],
+        [ContainerSensor(coordinator, container_id) for container_id in containers],
         update_before_add=True,
     )
